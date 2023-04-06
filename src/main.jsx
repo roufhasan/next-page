434e694594481955f6543314ce21607a6632c1e6
roufhasane <roufhasan5@gmail.comm>
2023-04-06 11:57:37 +0600

    header added

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Books from "./components/Books";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "books",
+        element: <Books />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
